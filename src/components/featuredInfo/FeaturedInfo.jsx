2000f570997af5bb71ec356b38d1ffa1e2e3f781
klesio Nascimento <klesio.dev@gmail.com>
2021-12-12 20:31:00 -0300

    finished styles featured

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -2,56 +2,63 @@ import React from "react";
 import "./featuredInfo.css";
 
 import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
-import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined';
-import LoyaltyOutlinedIcon from '@material-ui/icons/LoyaltyOutlined';
-import AccountBalanceOutlinedIcon from '@material-ui/icons/AccountBalanceOutlined';
+import PeopleAltOutlinedIcon from "@material-ui/icons/PeopleAltOutlined";
+import LoyaltyOutlinedIcon from "@material-ui/icons/LoyaltyOutlined";
+import AccountBalanceOutlinedIcon from "@material-ui/icons/AccountBalanceOutlined";
 export default function FeaturedInfo() {
   return (
     <div className="featured">
-      <div className="featuredItem  ">
-        <span className="featuredTitle">Unidades Registradas</span>
-        <div className="featuredRegisteredUnit">
+      <div className="featuredItem">
+        <div className="featureElements">
           <span className="featuredIcon">
             <StorefrontOutlinedIcon />
           </span>
-          <span className="featuredRate">1</span>
+          <div className="featuredRegisteredUnit">
+            <span className="featuredTitle">Unidades Registradas</span>
+            <span className="featuredRate">1</span>
+          </div>
         </div>
       </div>
 
       <div className="featuredItem  ">
-        <span className="featuredTitle">Clientes</span>
-        <div className="featuredRegisteredUnit">
+        <div className="featureElements">
           <span className="featuredIcon">
             <PeopleAltOutlinedIcon />
           </span>
-          <span className="featuredRate">104</span>
+          <div className="featuredRegisteredUnit">
+            <span className="featuredTitle">Clientes</span>
+            <span className="featuredRate">104</span>
+          </div>
         </div>
       </div>
 
       <div className="featuredItem  ">
-        <span className="featuredTitle">Vendas Realizadas</span>
-        <div className="featuredRegisteredUnit">
+        <div className="featureElements">
           <span className="featuredIcon">
             <LoyaltyOutlinedIcon />
           </span>
-          <span className="featuredRate">Vendas Realizadas</span>
+          <div className="featuredRegisteredUnit">
+            <span className="featuredTitle">Vendas Realizadas</span>
+            <span className="featuredRate">7002</span>
+          </div>
         </div>
       </div>
 
       <div className="featuredItem  ">
-        <span className="featuredTitle">Lucro Liquido (Geral)</span>
-        <div className="featuredRegisteredUnit">
+        <div className="featureElements">
           <span className="featuredIcon">
             <AccountBalanceOutlinedIcon />
           </span>
-          <span className="featuredRate">R$ 427.300,50</span>
+          <div className="featuredRegisteredUnit">
+            <span className="featuredTitle">Lucro Liquido(Geral)</span>
+            <span className="featuredRate">R$ 427.300,50</span>
+          </div>
         </div>
       </div>
 
       <div></div>
       <div></div>
       <div></div>
-
     </div>
   );
 }
